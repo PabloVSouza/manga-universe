910bca39c594125f6376f02e663bbdce35177f62
Pablo Souza <pablovsouza@gmail.com>
2020-10-23 02:42:04 -0300

    Internacionalização funcional e idioma Ingles adicionado

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,9 +1,10 @@
 import Vue from "vue"
 
 const ptBR = require("./ptBR")
+const enUS = require("./enUS")
 
 const defaultLanguage = "ptBR"
 
-const lang = { ptBR }
+const lang = { ptBR, enUS }
 
 Vue.prototype.$lang = lang[defaultLanguage]
